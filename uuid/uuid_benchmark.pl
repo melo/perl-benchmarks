ac1562c650645f3d4f9df74b82cc7f23d3775852
Pedro Melo <melo@simplicidade.org>
2012-07-24 17:37:09 +0100

    Updated UUID benchmark:
     * added Data::UUID::MT;
     * added binary and string representations;
     * focused on V3, V4 and V5 formats only;
     * tidy up a bit.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- uuid/uuid_benchmark.pl
+++ uuid/uuid_benchmark.pl
@@ -6,18 +6,30 @@ use Benchmark qw( cmpthese );
 use UUID::Tiny qw( :std );
 use Data::UUID;
 use Data::UUID::LibUUID;
+use Data::UUID::MT;
 
-my $du = Data::UUID->new;
+my $du  = Data::UUID->new;
+my $mt  = Data::UUID::MT->new;
+my $mtn = $mt->iterator;
 
 cmpthese(
   -2,
-  { uuid_tiny_v1 => sub { create_uuid_as_string(UUID_V1, 'asdfgh') },
-    uuid_tiny_3  => sub { create_uuid_as_string(UUID_V3, 'asdfgh') },
-    uuid_tiny_4  => sub { create_uuid_as_string(UUID_V4, 'asdfgh') },
-    uuid_tiny_5  => sub { create_uuid_as_string(UUID_V5, 'asdfgh') },
-    
-    data_uuid => sub { $du->create_str },
+  { uuid_tiny_3     => sub { create_uuid_as_string(UUID_V3, $$) },
+    uuid_tiny_3_bin => sub { create_uuid(UUID_V3, $$) },
+    uuid_tiny_4     => sub { create_uuid_as_string(UUID_V4) },
+    uuid_tiny_4_bin => sub { create_uuid(UUID_V4) },
+    uuid_tiny_5     => sub { create_uuid_as_string(UUID_V5, $$) },
+    uuid_tiny_5_bin => sub { create_uuid(UUID_V5, $$) },
 
-    data_uuid_libuuid => sub { new_uuid_string() },
+    data_uuid_3     => sub { $du->create_str },
+    data_uuid_3_bin => sub { $du->create_bin },
+
+    data_uuid_libuuid_2     => sub { new_uuid_string(2) },
+    data_uuid_libuuid_2_bin => sub { new_uuid_binary(2) },
+    data_uuid_libuuid_4     => sub { new_uuid_string(4) },
+    data_uuid_libuuid_4_bin => sub { new_uuid_binary(4) },
+
+    data_uuid_mt_4     => sub { $mt->create_string },
+    data_uuid_mt_4_bin => sub { $mtn->() },
   }
 );
