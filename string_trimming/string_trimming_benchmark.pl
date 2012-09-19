a3c5684be4a2b6e02efe0b301468e8c242837c2c
Yanick Champoux <yanick@babyl.dyndns.org>
2012-09-19 17:53:50 -0400

    trimming benchmark

--- string_trimming/string_trimming_benchmark.pl
+++ string_trimming/string_trimming_benchmark.pl
@@ -8,38 +8,76 @@ use Benchmark qw(:all);
 use String::Trim;
 use String::Strip;
 
-benchmark_string( "100 spaces",  join '', ' ' x 100, 'foo', ' ' x 100 );
-benchmark_string( "5 spaces",  join '', ' ' x 5, 'foo', ' ' x 5 );
+my %methods = (
+    'String::Trim'  => \&string_trim,
+    'cucumber_cuts' => \&cucumber,
+    'String::Strip' => \&string_strip,
+    'regex'         => \&regex,
+);
 
+benchmark_string( 
+    "100 spaces",  
+    join( '', ' ' x 100, 'foo', ' ' x 100  ),
+    'foo'
+);
+
+benchmark_string( 
+    "5 spaces",  
+    join( '', ' ' x 5, 'foo', ' ' x 5 ),
+    'foo'
+);
+
+benchmark_string( 
+    "no space",  
+    'foo',
+    'foo'
+);
+
+sub string_trim { 
+    return String::Trim::trim( shift );
+}
+
+sub regex { 
+    my $string = shift;
+    $string =~ s/^\s*|\s*$//g;
+    return $string;
+}
+
+sub cucumber {
+    my $string = shift;
+    local $/;
+    for my $x ( ' ', "\t", "\n" ) {
+        $/ = $x;
+        for( 1..2 ) { 1 while chomp $string; $string = reverse $string; }
+    }
+    return $string;
+}
+
+sub string_strip {
+    my $string = shift;
+    StripLTSpace($string);
+    return $string;
+}
 
 sub benchmark_string {
-    my( $title, $string ) = @_;
+    my( $title, $string, $result ) = @_;
 
-    say "## $title";
+    say "\n## $title";
 
-    cmpthese(
-    -2,
-    { 
-        'String::Trim' => sub { 
-            my $trimmed = String::Trim::trim( $string );
-        },
-        's/^\s*|\s*$//g' => sub { 
-            ( my $trimmed = $string ) =~ s/^\s*|\s*$//g;
-        },
-        'cucumber cuts' => sub {
-            my $trimmed = $string;
-            local $/;
-            for $/ ( ' ', "\t", "\n" ) {
-                    for( 1..2 ) { 1 while chomp $trimmed; $trimmed = reverse $trimmed; }
-            }
-            return $trimmed;
-        },
-        'String::Strip' => sub {
-            my $trimmed = $string;
-            StripLTSpace($trimmed);
-            die unless $trimmed eq 'foo';
-            return $trimmed;
-        },
+    my %m = %methods;
+
+    for ( keys %m ) {
+        my $f = $m{$_};
+        $m{$_} = sub { $f->($string) }
     }
-    );
+
+    # sanity check
+    if ( $result ) {
+        while ( my ( $t, $m ) = each %m ) {
+            my $r = $m->();
+            die "$t returned $r, not $result" if $r ne $result;
+        }
+    }
+
+    cmpthese( -2, \%m );
 }
