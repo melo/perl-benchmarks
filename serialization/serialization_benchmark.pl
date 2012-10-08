1f76e986c7d40466b29bc3f14071bb2d35d018d9
Steffen Mueller <smueller@cpan.org>
2012-10-08 21:06:25 +0200

    Add Sereal to serialization benchmark

--- serialization/serialization_benchmark.pl
+++ serialization/serialization_benchmark.pl
@@ -10,6 +10,11 @@ use Data::Dump        ();
 use Storable          ();
 use JSON::XS          ();
 use Data::MessagePack ();
+use Sereal::Encoder;
+use Sereal::Decoder;
+
+my $s_enc = Sereal::Encoder->new({no_shared_hashkeys => 1});
+my $s_dec = Sereal::Decoder->new;
 
 my $hash = {
   1 => {
@@ -23,6 +28,10 @@ my $hash = {
   },
 };
 
+# NOTE: Modules like Data::Dumper inadvertently upgrade the data structure
+#       that they are serializing. That means that subsequent serialization
+#       with more sophisticated tools is going to be slowed down artificially.
+#       Worse yet, it's dependend on the order in which things are benchmarked.
 my $t;
 cmpthese(
   -2,
@@ -31,6 +40,7 @@ cmpthese(
     'storable'     => sub { $t = Storable::nfreeze($hash) },
     'json_xs'      => sub { $t = JSON::XS::encode_json($hash) },
     'data_msgpack' => sub { $t = Data::MessagePack->pack($hash) },
+    'sereal      ' => sub { $t = $s_enc->encode($hash) },
   }
 );
 
@@ -44,11 +54,22 @@ cmpthese(
 # data_msgpack 271773/s       8730%       1642%      1553%           --        -6%
 # json_xs      289616/s       9309%       1756%      1661%           7%         --
 
+## Linux l4tsee 3.0.0-23-generic #39-Ubuntu SMP Thu Jul 19 19:19:11 UTC 2012 x86_64 x86_64 x86_64 GNU/Linux
+## Intel(R) Core(TM) i5 CPU       M 520  @ 2.40GHz
+#                   Rate      pp dumper storable data_msgpack json_xs sereal
+# pp              4419/s      --   -86%     -92%         -99%    -99%        -100%
+# dumper         30916/s    600%     --     -45%         -90%    -95%         -97%
+# storable       56659/s   1182%    83%       --         -82%    -91%         -94%
+# data_msgpack  315789/s   7045%   921%     457%           --    -51%         -69%
+# json_xs       645068/s  14496%  1987%    1039%         104%      --         -37%
+# sereal       1029248/s  23189%  3229%    1717%         226%     60%           --
+
 
 my $e1 = Data::Dumper::Dumper($hash);
 my $e2 = Storable::nfreeze($hash);
 my $e3 = JSON::XS::encode_json($hash);
 my $e4 = Data::MessagePack->pack($hash);
+my $e5 = $s_enc->encode($hash);
 
 cmpthese(
   -2,
@@ -56,6 +77,7 @@ cmpthese(
     'storable'     => sub { $t = Storable::thaw($e2) },
     'json_xs'      => sub { $t = JSON::XS::decode_json($e3) },
     'data_msgpack' => sub { $t = Data::MessagePack->unpack($e4) },
+    'sereal'       => sub { $t = $s_dec->decode($e5) },
   }
 );
 
@@ -67,3 +89,12 @@ cmpthese(
 # storable     125431/s         456%           --         -30%         -41%
 # data_msgpack 177977/s         689%          42%           --         -17%
 # json_xs      213608/s         847%          70%          20%           --
+
+## Linux l4tsee 3.0.0-23-generic #39-Ubuntu SMP Thu Jul 19 19:19:11 UTC 2012 x86_64 x86_64 x86_64 GNU/Linux
+## Intel(R) Core(TM) i5 CPU       M 520  @ 2.40GHz
+#                  Rate        eval   storable data_msgpack    json_xs      sereal
+# eval          36540/s          --       -83%         -88%       -90%        -92%
+# storable     209755/s        474%         --         -31%       -45%        -52%
+# data_msgpack 302207/s        727%        44%           --       -20%        -31%
+# json_xs      380075/s        940%        81%          26%         --        -13%
+# sereal       434834/s       1090%       107%          44%        14%          --
